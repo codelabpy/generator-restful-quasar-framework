36b9357e5ef799cc33fd24b7136aea9a00cfd950
Leonardo Benitez <benitez.leonardo.py@gmail.com>
2022-06-19 16:51:19 -0400

    Fix code formatin using eslint and standar guideline

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,55 +4,49 @@ const titleCase = require('title-case')
 const Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
-    
-    async prompting() {
+  async prompting () {
+    this.props = await this.prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter the project name:',
+        default: 'My Super Project'
+      }
+    ])
+  }
 
-        this.props = await this.prompt([
-           {
-                type:"input",
-                name: "projectName",
-                message: "Enter the project name:",
-                default: "My Super Project"
-            }
-        ])
+  writing () {
+    this.fs.copy(this.templatePath(), this.destinationPath(), {
+      globOptions: { dot: true }
+    })
 
+    const properties = {
+      projectName: this.props.projectName,
+      projectNameTitleCase: titleCase.titleCase(this.props.projectName),
+      projectNameParamCase: changeCase.paramCase(this.props.projectName)
     }
 
-    writing(){
-        
-        this.fs.copy(this.templatePath(), this.destinationPath(), {
-            globOptions: { dot: true }
-        });
+    const thisobj = this
+    function ctpl (tplname) {
+      thisobj.fs.copyTpl(
+        thisobj.templatePath(tplname),
+        thisobj.destinationPath(tplname),
+        properties
+      )
+    }
+    ctpl('package.json')
+    ctpl('README.md')
+    ctpl('index.html')
+    ctpl('quasar.config.js')
+    ctpl('src/pages/LoginPage.vue')
+    ctpl('src/layouts/MainLayout.vue')
+  }
 
-        const properties = {
-            projectName : this.props.projectName,
-            projectNameTitleCase : titleCase.titleCase(this.props.projectName),
-            projectNameParamCase: changeCase.paramCase(this.props.projectName)
-        }
-        
-        let thisobj = this;
-        function ctpl(tplname) {
-            thisobj.fs.copyTpl(
-                thisobj.templatePath(tplname),
-                thisobj.destinationPath(tplname),
-                properties
-            );
-        }
-        ctpl("package.json");
-        ctpl("README.md");
-        ctpl("index.html");
-        ctpl("quasar.config.js");
-        ctpl("src/pages/LoginPage.vue");
-        ctpl("src/layouts/MainLayout.vue");
+  install () {
+    if (this.options['skip-install']) {
+      return
     }
 
-    install() {
-        if (this.options["skip-install"]) {
-            return;
-        }
-
-        this.spawnCommand("yarn");
-    }
-
-
+    this.spawnCommand('yarn')
+  }
 }
